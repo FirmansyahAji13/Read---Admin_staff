8405631a71b3281413a63294bc639b1a48b50e84
FirmansyahAji13 <faji492@gmail.com>
2023-06-24 19:24:55 +0700

    Add project files.

---
+++ staff.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace JT_APPS__Final_Setting_
+{
+    partial class staff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(staff));
+            panelSidebar = new Panel();
+            BtnHomePage = new Button();
+            pictureBoxLogo = new PictureBox();
+            BtnWishlist = new Button();
+            BtnPaket = new Button();
+            btnHistory = new Button();
+            btnVoucher = new Button();
+            panel2 = new Panel();
+            PanelNavBar = new Panel();
+            label1 = new Label();
+            panel5 = new Panel();
+            PanelMain = new Panel();
+            button3 = new Button();
+            button4 = new Button();
+            panel4 = new Panel();
+            dataGridView1 = new DataGridView();
+            panel3 = new Panel();
+            textBox1 = new TextBox();
+            button5 = new Button();
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            panelSidebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
+            PanelNavBar.SuspendLayout();
+            PanelMain.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel3.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelSidebar
+            // 
+            panelSidebar.BackgroundImage = (Image)resources.GetObject("panelSidebar.BackgroundImage");
+            panelSidebar.Controls.Add(BtnHomePage);
+            panelSidebar.Controls.Add(pictureBoxLogo);
+            panelSidebar.Controls.Add(BtnWishlist);
+            panelSidebar.Controls.Add(BtnPaket);
+            panelSidebar.Controls.Add(btnHistory);
+            panelSidebar.Controls.Add(btnVoucher);
+            panelSidebar.Dock = DockStyle.Left;
+            panelSidebar.Location = new Point(0, 0);
+            panelSidebar.Margin = new Padding(4, 3, 4, 3);
+            panelSidebar.Name = "panelSidebar";
+            panelSidebar.Size = new Size(211, 827);
+            panelSidebar.TabIndex = 2;
+            // 
+            // BtnHomePage
+            // 
+            BtnHomePage.BackColor = Color.Transparent;
+            BtnHomePage.Cursor = Cursors.Hand;
+            BtnHomePage.FlatAppearance.BorderSize = 0;
+            BtnHomePage.FlatStyle = FlatStyle.Flat;
+            BtnHomePage.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold, GraphicsUnit.Pixel);
+            BtnHomePage.Location = new Point(0, 117);
+            BtnHomePage.Margin = new Padding(4, 3, 4, 3);
+            BtnHomePage.Name = "BtnHomePage";
+            BtnHomePage.Size = new Size(211, 37);
+            BtnHomePage.TabIndex = 12;
+            BtnHomePage.Text = "Hompage";
+            BtnHomePage.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxLogo
+            // 
+            pictureBoxLogo.BackColor = Color.Transparent;
+            pictureBoxLogo.Image = (Image)resources.GetObject("pictureBoxLogo.Image");
+            pictureBoxLogo.Location = new Point(77, 33);
+            pictureBoxLogo.Margin = new Padding(4, 3, 4, 3);
+            pictureBoxLogo.Name = "pictureBoxLogo";
+            pictureBoxLogo.Size = new Size(54, 35);
+            pictureBoxLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxLogo.TabIndex = 0;
+            pictureBoxLogo.TabStop = false;
+            // 
+            // BtnWishlist
+            // 
+            BtnWishlist.BackColor = Color.Transparent;
+            BtnWishlist.Cursor = Cursors.Hand;
+            BtnWishlist.FlatAppearance.BorderSize = 0;
+            BtnWishlist.FlatStyle = FlatStyle.Flat;
+            BtnWishlist.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold, GraphicsUnit.Pixel);
+            BtnWishlist.Location = new Point(0, 282);
+            BtnWishlist.Margin = new Padding(4, 3, 4, 3);
+            BtnWishlist.Name = "BtnWishlist";
+            BtnWishlist.Size = new Size(211, 37);
+            BtnWishlist.TabIndex = 11;
+            BtnWishlist.Text = "Wishlist";
+            BtnWishlist.UseVisualStyleBackColor = false;
+            // 
+            // BtnPaket
+            // 
+            BtnPaket.BackColor = Color.Transparent;
+            BtnPaket.Cursor = Cursors.Hand;
+            BtnPaket.FlatAppearance.BorderSize = 0;
+            BtnPaket.FlatStyle = FlatStyle.Flat;
+            BtnPaket.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold, GraphicsUnit.Pixel);
+            BtnPaket.Location = new Point(0, 157);
+            BtnPaket.Margin = new Padding(4, 3, 4, 3);
+            BtnPaket.Name = "BtnPaket";
+            BtnPaket.Size = new Size(211, 37);
+            BtnPaket.TabIndex = 8;
+            BtnPaket.Text = "Paket";
+            BtnPaket.UseVisualStyleBackColor = false;
+            // 
+            // btnHistory
+            // 
+            btnHistory.BackColor = Color.Transparent;
+            btnHistory.Cursor = Cursors.Hand;
+            btnHistory.FlatAppearance.BorderSize = 0;
+            btnHistory.FlatStyle = FlatStyle.Flat;
+            btnHistory.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold, GraphicsUnit.Pixel);
+            btnHistory.Location = new Point(0, 240);
+            btnHistory.Margin = new Padding(4, 3, 4, 3);
+            btnHistory.Name = "btnHistory";
+            btnHistory.Size = new Size(211, 37);
+            btnHistory.TabIndex = 10;
+            btnHistory.Text = "History";
+            btnHistory.UseVisualStyleBackColor = false;
+            // 
+            // btnVoucher
+            // 
+            btnVoucher.BackColor = Color.Transparent;
+            btnVoucher.Cursor = Cursors.Hand;
+            btnVoucher.FlatAppearance.BorderSize = 0;
+            btnVoucher.FlatStyle = FlatStyle.Flat;
+            btnVoucher.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold, GraphicsUnit.Pixel);
+            btnVoucher.Location = new Point(0, 198);
+            btnVoucher.Margin = new Padding(4, 3, 4, 3);
+            btnVoucher.Name = "btnVoucher";
+            btnVoucher.Size = new Size(211, 37);
+            btnVoucher.TabIndex = 9;
+            btnVoucher.Text = "Voucher";
+            btnVoucher.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(211, 0);
+            panel2.Margin = new Padding(4, 3, 4, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(6, 827);
+            panel2.TabIndex = 3;
+            // 
+            // PanelNavBar
+            // 
+            PanelNavBar.BackColor = Color.FromArgb(55, 163, 165);
+            PanelNavBar.BackgroundImageLayout = ImageLayout.Stretch;
+            PanelNavBar.Controls.Add(label1);
+            PanelNavBar.Controls.Add(panel5);
+            PanelNavBar.Dock = DockStyle.Top;
+            PanelNavBar.Location = new Point(217, 0);
+            PanelNavBar.Margin = new Padding(4, 3, 4, 3);
+            PanelNavBar.Name = "PanelNavBar";
+            PanelNavBar.Size = new Size(960, 73);
+            PanelNavBar.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 22F, FontStyle.Bold, GraphicsUnit.Pixel);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(381, 12);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(185, 26);
+            label1.TabIndex = 6;
+            label1.Text = "DAFTAR STAFF";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            panel5.Location = new Point(0, 68);
+            panel5.Margin = new Padding(4, 3, 4, 3);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(961, 758);
+            panel5.TabIndex = 3;
+            // 
+            // PanelMain
+            // 
+            PanelMain.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            PanelMain.AutoScroll = true;
+            PanelMain.BackColor = SystemColors.ButtonHighlight;
+            PanelMain.Controls.Add(button3);
+            PanelMain.Controls.Add(button4);
+            PanelMain.Controls.Add(panel4);
+            PanelMain.Controls.Add(panel3);
+            PanelMain.Controls.Add(panel1);
+            PanelMain.Location = new Point(216, 68);
+            PanelMain.Margin = new Padding(4, 3, 4, 3);
+            PanelMain.Name = "PanelMain";
+            PanelMain.Size = new Size(961, 758);
+            PanelMain.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top;
+            button3.BackColor = Color.FromArgb(6, 215, 115);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Pixel);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(586, 660);
+            button3.Margin = new Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(160, 50);
+            button3.TabIndex = 10;
+            button3.Text = "HAPUS";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top;
+            button4.BackColor = Color.FromArgb(6, 215, 115);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Pixel);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(779, 660);
+            button4.Margin = new Padding(4, 3, 4, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(160, 50);
+            button4.TabIndex = 11;
+            button4.Text = "SIMPAN";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            panel4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel4.Controls.Add(dataGridView1);
+            panel4.Location = new Point(24, 218);
+            panel4.Margin = new Padding(4, 3, 4, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(914, 395);
+            panel4.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Margin = new Padding(4, 3, 4, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(914, 395);
+            dataGridView1.TabIndex = 13;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // panel3
+            // 
+            panel3.Anchor = AnchorStyles.Top;
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(button5);
+            panel3.Location = new Point(20, 27);
+            panel3.Margin = new Padding(4, 3, 4, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(919, 98);
+            panel3.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Pixel);
+            textBox1.ForeColor = SystemColors.WindowFrame;
+            textBox1.Location = new Point(27, 22);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(816, 19);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Search";
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(851, 27);
+            button5.Margin = new Padding(4, 3, 4, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(36, 37);
+            button5.TabIndex = 0;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(21, 132);
+            panel1.Margin = new Padding(4, 3, 4, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(923, 53);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top;
+            button1.BackColor = Color.FromArgb(55, 163, 165);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Pixel);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(721, 0);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 50);
+            button1.TabIndex = 9;
+            button1.Text = "EDIT DATA";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top;
+            button2.BackColor = Color.FromArgb(55, 163, 165);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Pixel);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(4, 0);
+            button2.Margin = new Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(197, 50);
+            button2.TabIndex = 8;
+            button2.Text = "TAMBAH DATA";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // staff
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1177, 827);
+            Controls.Add(PanelMain);
+            Controls.Add(PanelNavBar);
+            Controls.Add(panel2);
+            Controls.Add(panelSidebar);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "staff";
+            Text = "UbahSandi";
+            WindowState = FormWindowState.Maximized;
+            panelSidebar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
+            PanelNavBar.ResumeLayout(false);
+            PanelNavBar.PerformLayout();
+            PanelMain.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelSidebar;
+        private Button BtnHomePage;
+        private PictureBox pictureBoxLogo;
+        private Button BtnWishlist;
+        private Button BtnPaket;
+        private Button btnHistory;
+        private Button btnVoucher;
+        private Panel panel2;
+        private Panel PanelNavBar;
+        private Panel panel5;
+        private Panel PanelMain;
+        private Label label1;
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private Panel panel3;
+        private TextBox textBox1;
+        private Button button5;
+        private Button button3;
+        private Button button1;
+        private Button button2;
+        private Button button4;
+        private Panel panel4;
+    }
+}
---
+++ staff.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace JT_APPS__Final_Setting_
+{
+    public partial class staff : Form
+    {
+        private const string connectionString = "Server=localhost; Port=5432; Database=JT-Apps; User Id=postgres; Password=timotius";
+        public staff()
+        {
+            InitializeComponent();
+            LoadDataToDataGridView();
+        }
+        private void LoadDataToDataGridView()
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                using (NpgsqlCommand comm = new NpgsqlCommand())
+                {
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.Text;
+                    comm.CommandText = "SELECT * FROM staff";
+                    NpgsqlDataReader dataReader = comm.ExecuteReader();
+
+                    if (dataReader.HasRows)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(dataReader);
+                        dataGridView1.DataSource = dt;
+                    }
+
+                    dataReader.Close();
+                }
+            }
+        }
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnGanti_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Apakah anda yakin ingin mengganti sandi?", "Ubah Sandi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
